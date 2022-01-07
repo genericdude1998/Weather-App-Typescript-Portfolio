5781e8af9e9cedef4b94c84978a5ce49c62ac941
vsenzo <vsenzo@hello.com>
2022-01-07 12:53:00 +0000

    added css temp

--- src/Temperature.tsx
+++ src/Temperature.tsx
@@ -1,20 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import {WiCelsius} from 'react-icons/wi';
 
 interface ITempProps {
     temp: string,
 }
 
+const Container = styled.div`
+    display:flex;
+    align-items:center;
+`
 
 const StyledTemp = styled.h1`
     font-size: 10vh;
-    margin:-20px;
+    margin-right:-10px;
+`
+
+const StyledIconTemp = styled(WiCelsius)`
+    margin-left:-0px;
 `
 
 class Temperature extends React.Component<ITempProps> {
     public render() {
         return(
-            <StyledTemp>{this.props.temp}</StyledTemp>
+            <Container>
+                <StyledTemp>{this.props.temp}</StyledTemp>
+                <StyledIconTemp size={50}/>
+            </Container>
         );
     }
 }
